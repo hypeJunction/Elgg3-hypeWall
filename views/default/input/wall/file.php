2b7f6ad587342d72dc45ab5feda7cbb5895cc82c
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2018-04-20 00:33:03 +0200

    fix(form): correctly render file input

--- views/default/input/wall/file.php
+++ views/default/input/wall/file.php
@@ -3,10 +3,9 @@
 $name = elgg_extract('name', $vars);
 if (elgg_view_exists('input/dropzone')) {
 	echo elgg_view('input/dropzone', [
-		'accept' => "image/*",
+		//'accept' => "image/*",
 		'max' => 25,
 		'multiple' => true,
-		'action' => elgg_normalize_url('action/dropzone/upload'),
 	] + $vars);
 } else {
 	$vars['name'] = "{$name}[]";
