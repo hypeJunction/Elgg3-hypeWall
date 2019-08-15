3d10e0120bd09a83a909e8f43f9c1ac2eba39db7
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2019-08-15 17:16:32 +0200

    fix(views): use default object view

--- views/default/object/hjwall.php
+++ views/default/object/hjwall.php
@@ -7,62 +7,28 @@ if (!$entity instanceof Post) {
 	return;
 }
 
-$poster = $entity->getOwnerEntity();
-$wall_owner = $entity->getContainerEntity();
+$params = $vars;
 
-$content = '';
+$poster = $entity->getOwnerEntity();
 
 $message = $entity->formatMessage();
 if ($message) {
-	$content .= elgg_format_element('div', [
+	$params['content'] = elgg_format_element('div', [
 		'class' => 'wall-message',
 			], $message);
 }
 
 $attachments = $entity->formatAttachments();
 if ($attachments) {
-	$content .= elgg_format_element('div', [
+	$vars['attachments'] = elgg_format_element('div', [
 		'class' => 'wall-attachments',
 			], $attachments);
 }
 
-$menu = '';
-if (!elgg_in_context('widgets')) {
-	$menu = elgg_view_menu('entity', [
-		'entity' => $entity,
-		'handler' => 'wall',
-		'sort_by' => 'priority',
-		'class' => 'elgg-menu-hz',
-	]);
-}
-
-$subtitle = $entity->formatSummary();
-$user_icon = elgg_view_entity_icon($poster, 'medium', [
-	'use_hover' => false,
-	'img_class' => 'wall-poster-avatar'
-		]);
+$vars['subtitle'] = $entity->formatSummary();
 
 if (elgg_extract('full_view', $vars, false)) {
-	$summary = elgg_view('object/elements/summary', [
-		'entity' => $entity,
-		'title' => false,
-		'metadata' => $menu,
-		'subtitle' => $subtitle,
-	]);
-
-	echo elgg_view('object/elements/full', [
-		'entity' => $entity,
-		'summary' => $summary,
-		'icon' => $icon,
-		'body' => $content . elgg_view_comments($entity),
-	]);
+	echo elgg_view('object/elements/full', $params);
 } else {
-	echo elgg_view('object/elements/summary', [
-		'entity' => $entity,
-		'title' => false,
-		'metadata' => $menu,
-		'subtitle' => $subtitle,
-		'content' => $content,
-		'icon' => $user_icon,
-	]);
+	echo elgg_view('object/elements/summary', $params);
 }
